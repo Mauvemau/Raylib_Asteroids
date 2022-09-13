70ec18464b5f39af07b04d84282a98e498325eed
Mau <mdamiansalazar@gmail.com>
2022-09-13 12:17:14 -0300

    Added TODO list.

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,8 +1,23 @@
 #include "Game.h"
+
 #include "raylib.h"
 
+#include <iostream>
+
 namespace Game {
+	Vector2 positionToMove;
+
+	void ManageInput();
 	void Draw();
+
+	void ManageInput() {
+		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+			positionToMove = GetMousePosition();
+			std::cout << "Moving towards (" << positionToMove.x << ";" << positionToMove.y << ")\n";
+		}
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+			std::cout << "Shooting!\n";
+	}
 
 	void Draw() {
 		BeginDrawing();
@@ -12,6 +27,7 @@ namespace Game {
 	}
 
 	void Update() {
+		ManageInput();
 		Draw();
 	}
 
