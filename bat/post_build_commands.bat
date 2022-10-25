17e06c4caf236acd25ba16d8c61c6f209c4ccda9
Mau <mauvepinktre@gmail.com>
2022-10-25 11:07:26 -0300

    Created batch file for post_build commands

---
+++ bat/post_build_commands.bat
@@ -0,0 +1,6 @@
+@echo off
+set solution_dir=%~1
+set config=%~2
+xcopy /e /v /y %solution_dir%\resources %solution_dir%\bin\%config%\resources\
+cd %solution_dir%\
+tar --create --file=Game_Build.zip bin
