41f56f9e03bf251d283e235ee42e4d221647f6b3
Mau <mauvepinktree@gmail.com>
2022-11-23 19:46:23 -0300

    Modified batch file once more, I forgot to add quotation marks to the last command

--- bat/post_build_commands.bat
+++ bat/post_build_commands.bat
@@ -2,5 +2,5 @@
 set solution_dir=%~1
 set config=%~2
 xcopy /e /v /y "%solution_dir%\resources" "%solution_dir%\bin\%config%\resources\"
-cd %solution_dir%\
+cd "%solution_dir%\"
 tar --create --file=Game_Build.zip bin
